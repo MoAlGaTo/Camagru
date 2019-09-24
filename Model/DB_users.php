f5c4b29f0a137264d1a18e4a2807722b5293a5bf
Mohamed Ali <moal.gadacha@gmail.com>
2019-09-24 16:59:02 +0200

    users_database

--- Model/DB_users.php
+++ Model/DB_users.php
@@ -1,15 +1,20 @@
 <?php
-
+error_reporting(E_ALL);
 require_once("Model/DB_connect.php");
 
 class user
 {
     $db = new DataBase;
 
-    public function add_user()
+    public function add_user($lastname, $firstname, $pseudonym, $email, $passworduser)
     {
        $db->prepare('INSERT INTO users (lastname, firstname, pseudonym, email, passworduser)
-        VALUES ()');
+        VALUES ($lastname, $firstname, $pseudonym, $email, $passworduser)');
+    }
+
+    public function edit_pseudo($pseudonym, $id)
+    {
+        $db->exec('UPDATE users SET pseudonym=$pseudonym  WHERE ID=$id')
     }
 }
 
