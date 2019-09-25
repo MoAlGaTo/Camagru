15db2ce7bd7fb7665a0808fe20ce69de40f7cdc5
Mohamed Ali <moal.gadacha@gmail.com>
2019-09-25 18:42:12 +0200

    PDO-PDOStatement

--- Model/DB_users.php
+++ Model/DB_users.php
@@ -1,20 +1,23 @@
 <?php
-error_reporting(E_ALL);
-require_once("Model/DB_connect.php");
+
+require_once("DB_connect.php");
 
 class user
 {
-    $db = new DataBase;
 
     public function add_user($lastname, $firstname, $pseudonym, $email, $passworduser)
     {
-       $db->prepare('INSERT INTO users (lastname, firstname, pseudonym, email, passworduser)
-        VALUES ($lastname, $firstname, $pseudonym, $email, $passworduser)');
+        $db = new DataBase;
+
+        $statement = $db->prepare('INSERT INTO users (lastname, firstname, pseudonym, email, passworduser)
+        VALUES (:lastname, :firstname, :pseudonym, :email, :passworduser)');
     }
 
     public function edit_pseudo($pseudonym, $id)
     {
-        $db->exec('UPDATE users SET pseudonym=$pseudonym  WHERE ID=$id')
+        $db = new DataBase;
+
+        $statement = $db->prepare('UPDATE users SET pseudonym=:pseudonym  WHERE ID=:id');
     }
 }
 
