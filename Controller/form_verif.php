d054e07f8b3d2935b1f3639a7a7f983ed6442f0a
Mohamed Ali <moal.gadacha@gmail.com>
2019-10-01 18:12:08 +0200

    Regex

---
+++ Controller/form_verif.php
@@ -0,0 +1,9 @@
+<?php
+
+$lastname_verif = "#^[a-zA-Z]+[a-zA-Z -]?$#";
+$firstname_verif = "#^[a-zA-Z]+[a-zA-Z -]?$#";
+$mail_verif = "#^[0-9a-zA-Z-_\.]+@[a-zA-Z-_\.]{2,}.[a-zA-Z]{2,4}$#";
+$pseudo_verif = NULL;
+$password_verif = NULL;
+
+?>
